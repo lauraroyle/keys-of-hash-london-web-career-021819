951896a5d19cf10726ad8b72bdf4513d1c4a21f1
Laura Royle <lauraamie.royle@gmail.com>
2019-02-14 09:03:47 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,10 +3,10 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     array = []
-binding.pry
+
     arguments.each do |argument|
       self.each do |key, value|
-
+        
       end
     end
   end

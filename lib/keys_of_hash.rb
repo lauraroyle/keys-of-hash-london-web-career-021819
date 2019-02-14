81028851f779e1ae7df841b165f5f7133191d759
Laura Royle <lauraamie.royle@gmail.com>
2019-02-14 09:01:05 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -10,6 +10,3 @@ binding.pry
   end
 end
 
-#method should take an undefined number of arguments, using the splat operator
-
-#return an array with every key from the hash whose value matches the value(s) given as an argument. .keys

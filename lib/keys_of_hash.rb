7cb05ec29844fc97e684500a1a7633601f63900f
Laura Royle <lauraamie.royle@gmail.com>
2019-02-14 09:03:11 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,8 @@ class Hash
   def keys_of(*arguments)
     array = []
 binding.pry
-    arguments.each do |arguments|
+    arguments.each do |argument|
+      self.each do |key, value|
 
     end
   end
